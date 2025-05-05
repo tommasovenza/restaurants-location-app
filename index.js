0ed24e4e9b555a8f006c48a96f052bbb539a61f1
tommasovenza <tommasovenza@gmail.com>
2025-05-05 17:29:19 +0200

    created swipe functionality

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('first commit')
+const menu = document.querySelector('#menu')
+const container = document.querySelector('#app')
+
+function swipe(e) {
+  if (
+    e.target.classList.contains('rest-stars') ||
+    e.target.classList.contains('rest-tags') ||
+    e.target.classList.contains('title') ||
+    e.target.classList.contains('rest-texts') ||
+    e.target.classList.contains('fa-solid') ||
+    e.target.classList.contains('distance-desc') ||
+    e.target.classList.contains('tag') ||
+    e.target.classList.contains('rest-pic') ||
+    e.target.classList.contains('icon-distance') ||
+    e.target.classList.contains('row')
+  ) {
+    e.target.closest('.row').classList.toggle('active')
+  }
+
+  console.log(e.target)
+}
+
+function init() {
+  container.addEventListener('click', swipe)
+}
+
+document.addEventListener('DOMContentLoaded', init)

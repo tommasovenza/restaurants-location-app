836392938140aeb8e2d51fc26316986ff588f232
tommasovenza <tommasovenza@gmail.com>
2025-05-06 10:51:27 +0200

    insert bookmarked functionality

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function swipe(e) {
     e.target.classList.contains('rest-tags') ||
     e.target.classList.contains('title') ||
     e.target.classList.contains('rest-texts') ||
-    e.target.classList.contains('fa-solid') ||
+    e.target.classList.contains('fa-arrows-turn-right') ||
     e.target.classList.contains('distance-desc') ||
     e.target.classList.contains('tag') ||
     e.target.classList.contains('rest-pic') ||
@@ -21,7 +21,24 @@ function swipe(e) {
       .classList.toggle('active')
   }
 
-  console.log(e.target)
+  // changing bookmark status
+  if (
+    e.target.classList.contains('fa-bookmark') ||
+    e.target.classList.contains('bookmark') ||
+    e.target.classList.contains('span-bookmark')
+  ) {
+    const bookmark = e.target
+      .closest('.bookmark')
+      .querySelector('i.fa-bookmark')
+
+    if (bookmark.classList.contains('fa-regular')) {
+      bookmark.classList.remove('fa-regular')
+      bookmark.classList.add('fa-solid')
+    } else {
+      bookmark.classList.remove('fa-solid')
+      bookmark.classList.add('fa-regular')
+    }
+  }
 }
 
 function init() {
